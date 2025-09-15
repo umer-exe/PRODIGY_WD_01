857a7fdece752353cba6160d1cd5cf1a5610547f
umer <umermalik2004@gmail.com>
2025-09-15 23:24:09 +0500

    responsive landing page - visual tweaks

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
 //navbar change on scroll
 window.addEventListener("scroll", () => {
   const navbar = document.querySelector(".navbar");
-  if (window.scrollY > 10) {      // small threshold
+  if (window.scrollY > 10) {
     navbar.classList.add("scrolled");
   } else {
     navbar.classList.remove("scrolled");
   }
 });
-
